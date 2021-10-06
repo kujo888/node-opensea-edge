f58754d5839d07d59524b6df83e6e936fcc1f178
kujo888 <888kujo888@gmail.com>
2021-10-07 01:25:28 +0900

    Update Test

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 var http = require('http');
 const { CronJob } = require('cron');
 
-const job = new CronJob(`*/5 * * * * *`, () => {
-  console.log(new Date());
-});
-
-job.start();
-
 http.createServer(function (req, res) {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('Bot is running!');
   
 }).listen(5000);
+
+const job = new CronJob(`*/5 * * * * *`, () => {
+  console.log(new Date());
+});
+
+job.start();

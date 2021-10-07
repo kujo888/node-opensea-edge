9d41740b1218a2f92e6e2fc255b7d32612f9464d
kujo888 <888kujo888@gmail.com>
2021-10-08 05:25:03 +0900

    Completed Everything

--- index.js
+++ index.js
@@ -24,7 +24,6 @@ const {
 http.createServer(function (req, res) {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('Bot is running!');
-
 }).listen(process.env.PORT || 3000);
 
 console.log(`RPC URL: \t\t ${RPC_URL}`);
